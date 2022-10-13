9827a4b02951e33e5254e60f2388cf289cf98da9
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-13 12:40:24 -0700

    amended 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,5 +11,7 @@ int main(void)
 	printf("The size of the int data type  is %d byte(s)\n", sizeof(int));
 	printf("The size of the float data type is %d byte(s)\n", sizeof(float));
 	printf("The size of the double data type is %d byte(s)", sizeof(double));
+	printf("The size of the long int data type is %d byte(s)", sizeof(long int));
+	printf("Size of long long int type is %d byte(s)", sizeof(long long int));
 	return (0);
 }
