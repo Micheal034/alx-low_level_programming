c019662b7960dbca862d1fb7595935e8948da6f0
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-13 12:31:42 -0700

    amended 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,15 @@
 #include "stdio.h"
 
 /**
-* main - prints the size of various types on the computer it is compiled and run on.
+* main - prints the size of various data types
 * Return: Always 0 (Successful)
 */
 
 int main(void)
 {
-	printf("The size of the data type char is %d\n", sizeof(char));
+	printf("The size of the char data type  is %d byte(s)\n", sizeof(char));
+	printf("The size of the int data type  is %d byte(s)\n", sizeof(int));
+	printf("The size of the float data type is %d byte(s)\n", sizeof(float));
+	printf("The size of the double data type is %d byte(s)", sizeof(double));
 	return (0);
 }
