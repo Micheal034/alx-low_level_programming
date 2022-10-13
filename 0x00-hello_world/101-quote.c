8073ffb94fae9b02a5d33e1f23e7e166809d81a1
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-13 13:09:12 -0700

    created 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+/**
+* main - prints sequence of strings
+* Return: Always 0 (Successful)
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
