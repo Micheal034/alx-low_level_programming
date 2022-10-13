35ed88bd2a8cde8b38371877eddf67952f919f1a
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-13 12:11:19 -0700

    created 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+/**
+* main- printing sequence of characters
+* Return: Always 0 (Successful)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
