c3cb0660c4c718300a1353d2bdb90a0e4d500b9e
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-13 12:21:35 -0700

    created 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+/**
+* main - prints the size of various types on the computer it is compiled and run on.
+* Return: Always 0 (Successful)
+*/
+
+int main(void)
+{
+	printf("The size of the data type char is %d\n", sizeof(char));
+	return (0);
+}
