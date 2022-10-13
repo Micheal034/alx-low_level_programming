73cad43f66995e9dbdf300a891139a305e79fea9
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-13 12:55:08 -0700

    amend 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
 
 int main(void)
 {
-	printf("The size of the char data type  is %d byte(s)\n", sizeof(char));
-	printf("The size of the int data type  is %d byte(s)\n", sizeof(int));
-	printf("The size of the float data type is %d byte(s)\n", sizeof(float));
-	printf("The size of the double data type is %d byte(s)", sizeof(double));
-	printf("The size of the long int data type is %d byte(s)", sizeof(long int));
-	printf("Size of long long int type is %d byte(s)", sizeof(long long int));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
