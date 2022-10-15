01fd8b937598c90d3436f5a1a5defc88157f2b1b
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-15 12:54:20 -0700

    amended 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,13 +5,12 @@
 */
 int main(void)
 {
-	char ch, ch_f, ch_r;
+	char ch;
 
-	for (ch = 'a', ch_f = 'f', ch_r = 'r'; ch < 'e', ch_f < 'q', ch_r <= 'z'; ch++, ch_f++, ch_r++)
+	for (ch = 'a' ; ch <= 'z'; ch++)
 	{
-		putchar(ch);
-		putchar(ch_f);
-		putchar(ch_r);
+		if ((ch != 'e') && (ch != 'q'))
+			putchar(ch);
 	}
 
 	putchar('\n');
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+/**
+* main-prints all possible combination of single digit numbers
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+	
