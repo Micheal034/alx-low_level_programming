baf03f4d5149a09aad2de105e252627a8f28b8b1
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-14 17:35:37 -0700

    amended 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,32 +5,10 @@
 */
 int main(void)
 {
-	putchar('a');
-	putchar('b');
-	putchar('c');
-	putchar('d');
-	putchar('e');
-	putchar('f');
-	putchar('g');
-	putchar('h');
-	putchar('i');
-	putchar('j');
-	putchar('k');
-	putchar('l');
-	putchar('m');
-	putchar('n');
-	putchar('o');
-	putchar('p');
-	putchar('q');
-	putchar('r');
-	putchar('s');
-	putchar('t');
-	putchar('u');
-	putchar('v');
-	putchar('w');
-	putchar('x');
-	putchar('y');
-	putchar('z');
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
