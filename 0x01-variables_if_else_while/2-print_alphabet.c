922c07da52a0162138e3c267e8caf15165a77212
Micheal034 <okechukwumicheal034@gmail.com>
2022-10-14 17:21:55 -0700

    amended 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+/**
+* main - prints alphabets in lowercase
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y');
+	putchar('z');
+	putchar('\n');
+	return (0);
+}
